3c71525c794eca2cff24a1bd32cd96a50dc8edbd
JoseArma <vencearma@gmail.com>
2021-04-24 21:56:24 -0500

    agregando estilos y botones

---
+++ src/main.js
@@ -0,0 +1,59 @@
+const placeh = document.getElementById('inputWindow')
+const button1 = document.getElementById('number1')
+const button2 = document.getElementById('number2')
+const button3 = document.getElementById('number3')
+const button4 = document.getElementById('number4')
+const button5 = document.getElementById('number5')
+const button6 = document.getElementById('number6')
+const button7 = document.getElementById('number7')
+const button8 = document.getElementById('number8')
+const button9 = document.getElementById('number9')
+const button0 = document.getElementById('number0')
+const point = document.getElementById('point')
+    /* 
+    ---------------BOTONES DE OPERACIONES */
+
+const sum = document.getElementById('buttonSum')
+const rests = document.getElementById('buttonRest')
+const multiplication = document.getElementById('buttonMulti')
+const division = document.getElementById('buttonDivi')
+const resultant = document.getElementById('buttonResult')
+const clean = document.getElementById('clear')
+
+
+/* function ModifyPlaceHolder(num) {
+    placeh.placeholder = "num";
+    let num = 
+} 
+button1.addEventListener("click", ModifyPlaceHolder)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+function saludo() {
+    console.log("sifunciona")
+}
+
+button1.addEventListener("click", saludo) */
